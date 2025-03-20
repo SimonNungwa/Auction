1d68371842008b43ead54408c59d90c79a9b271d
Simon <simonnungwa99@gmail.com>
2025-03-20 09:56:02 +0100

    update config file

---
+++ config/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    DEBUG = os.getenv('DEBUG', 'TRUE').lower() in ['true', '1']
+    PORT = int(os.getenv('PORT', 2024))
+
+config = Config()
+
+# TODO: fix the issues with the params
