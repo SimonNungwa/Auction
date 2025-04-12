bae019fcde526d5dcc89be6bc7ce071fdd68a5b5
Simon <simonnungwa99@gmail.com>
2025-04-12 21:38:35 +0100

    Empty commit to trigger build

---
+++ auction/__main__.py
@@ -0,0 +1,9 @@
+from app import app
+from config.config import config
+from auth import auth
+
+ 
+if __name__ == '__main__':
+    app.run(debug=config.DEBUG, port=config.PORT)
+
+# TODO: Import config file variables 
