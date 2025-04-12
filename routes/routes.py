d15ae9a220fbf90990dda57410455a67ef0ab59f
Simon <simonnungwa99@gmail.com>
2025-04-12 21:39:48 +0100

    Empty commit to trigger build

---
+++ routes/routes.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, url_for, redirect
+from app import app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/recover')
+def recover():
+    return render_template('recover.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/search')
+def search():
+    return render_template('searchPage.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
