25f7d9192b6d28ef49fe55fae28dd2ba8d7af51b
Simon <simonnungwa99@gmail.com>
2025-04-15 00:42:12 +0100

    added blueprints and registered routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
-
+from routes.routes import routes  # Import routes from the routes module
 
 app = Flask(__name__)
+
+# Register the Blueprint with the app
+app.register_blueprint(routes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
