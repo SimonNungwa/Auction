834692bba250d33994ac97bc67b9b205d962af2c
Simon <simonnungwa99@gmail.com>
2025-04-14 23:51:45 +0100

    updated auth

--- auth/auth.py
+++ auth/auth.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from flask_login import LoginManager
-from models.models import User, db
+from models.models import User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI 
-app.config['SECRET_KEY'] = 'your-secret-key'  
 
-db.init_app(app)
 login_manager = LoginManager()
+
 login_manager.init_app(app)
-login_manager.login_view = 'login' 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
+
